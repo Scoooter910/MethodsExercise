4068bf80edb155408bbdda1cb9596288f65fb156
Michele Mostoller <MMostoller@att.net>
2024-06-03 11:57:38 -0400

    Exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi,{userName}. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awesome color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! Now What is your favorite band?");
+            var band = Console.ReadLine();
+
         }
     }
 }
